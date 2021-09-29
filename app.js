5101a52519bfca90b4189d15fa89a72ceb62809f
Sravanthi <88923475+sravanthi-nandikonda@users.noreply.github.com>
2021-09-29 11:31:56 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
-const inputBox = document.querySelector(".inputField input");
-const addBtn = document.querySelector(".inputField button");
-const todoList = document.querySelector(".todoList");
-const deleteAllBtn = document.querySelector(".footer button");
+showtask();
+
+
+let addtaskinput = document.getElementById("addtaskinput");
+let addtaskbtn = document.getElementById("addtaskbtn");
 const dateElement=document.getElementById("date");
 
 
@@ -10,60 +11,193 @@ const today=new Date();
 
 dateElement.innerHTML= today.toLocaleDateString("en-Us",options);
 
-inputBox.onkeyup = ()=>{
-  let userEnteredValue = inputBox.value; 
-  if(userEnteredValue.trim() != 0){ 
-    addBtn.classList.add("active"); 
-  }else{
-    addBtn.classList.remove("active"); 
-  }
-}
-showTasks(); 
-addBtn.onclick = ()=>{ 
-  let userEnteredValue = inputBox.value; 
-  let getLocalStorageData = localStorage.getItem("New Todo"); 
-  if(getLocalStorageData == null){ 
-    listArray = []; 
-  }else{
-    listArray = JSON.parse(getLocalStorageData);  
-  }
-  listArray.push(userEnteredValue);
-  localStorage.setItem("New Todo", JSON.stringify(listArray));
-  showTasks(); 
-  addBtn.classList.remove("active"); 
-}
-function showTasks(){
-  let getLocalStorageData = localStorage.getItem("New Todo");
-  if(getLocalStorageData == null){
-    listArray = [];
-  }else{
-    listArray = JSON.parse(getLocalStorageData); 
-  }
-  const pendingTasksNumb = document.querySelector(".pendingTasks");
-  pendingTasksNumb.textContent = listArray.length;
-  if(listArray.length > 0){ 
-    deleteAllBtn.classList.add("active"); 
-  }else{
-    deleteAllBtn.classList.remove("active");
-  }
-  let newLiTag = "";
-  listArray.forEach((element, index) => {
-    newLiTag += `<li>${element}<span class="icon" onclick="deleteTask(${index})"><i class="fas fa-trash"></i></span></li>`;
-  });
-  todoList.innerHTML = newLiTag; 
-  inputBox.value = "";
+
+
+addtaskbtn.addEventListener("click", function(){
+    addtaskinputval = addtaskinput.value;
+    if(addtaskinputval.trim()!=0){
+        let webtask = localStorage.getItem("localtask");
+        if(webtask == null){
+            taskObj = [];
+        }
+        else{
+            taskObj = JSON.parse(webtask);
+        }
+        taskObj.push({'task_name':addtaskinputval, 'completeStatus':false});
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+        addtaskinput.value = '';
+    }
+    showtask();
+})
+
+
+// showtask
+function showtask(){
+    
+    let webtask = localStorage.getItem("localtask");
+    if(webtask == null){
+        taskObj = [];
+    }
+    else{
+        taskObj = JSON.parse(webtask);
+    }
+    let html = '';
+    let addedtasklist = document.getElementById("addedtasklist");
+    taskObj.forEach((item, index) => {
+
+        if(item.completeStatus==true){
+            taskCompleteValue = `<td class="completed">${item.task_name}</td>`;
+        }else{
+            taskCompleteValue = `<td>${item.task_name}</td>`;
+        }
+        html += `<tr>
+                    <th scope="row">${index+1}</th>
+                    ${taskCompleteValue}
+                    <td><button type="button" onclick="edittask(${index})" class="text-primary"><i class="fa fa-edit"></i>Edit</button></td>
+                    <td><button type="button" class="text-success" id=${index}><i class="far fa-check-circle"></i>Complete</button></td>
+                    <td><button type="button" onclick="deleteitem(${index})" class="text-danger"><i class="fa fa-trash"></i>Delete</button></td>
+                </tr>`;
+    });
+    addedtasklist.innerHTML = html;
 }
 
-function deleteTask(index){
-  let getLocalStorageData = localStorage.getItem("New Todo");
-  listArray = JSON.parse(getLocalStorageData);
-  listArray.splice(index, 1); //delete or remove the li
-  localStorage.setItem("New Todo", JSON.stringify(listArray));
-  showTasks(); //call the showTasks function
+// edittask
+function edittask(index){
+    let saveindex = document.getElementById("saveindex");
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let savetaskbtn = document.getElementById("savetaskbtn");
+    saveindex.value = index;
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask); 
+    
+    addtaskinput.value = taskObj[index]['task_name'];
+    addtaskbtn.style.display="none";
+    savetaskbtn.style.display="block";
 }
 
-deleteAllBtn.onclick = ()=>{
-  listArray = []; 
-  localStorage.setItem("New Todo", JSON.stringify(listArray));
-  showTasks(); 
+// savetask
+let savetaskbtn = document.getElementById("savetaskbtn");
+savetaskbtn.addEventListener("click", function(){
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask); 
+    let saveindex = document.getElementById("saveindex").value;
+    
+    for (keys in taskObj[saveindex]) {
+        if(keys == 'task_name'){
+            taskObj[saveindex].task_name = addtaskinput.value;
+        }
+      }
+    
+    savetaskbtn.style.display="none";
+    addtaskbtn.style.display="block";
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    addtaskinput.value='';
+    showtask();
+})
+// deleteitem
+function deleteitem(index){
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    taskObj.splice(index, 1);
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
 }
+
+
+
+// complete task
+let addedtasklist = document.getElementById("addedtasklist");
+    addedtasklist.addEventListener("click", function(e){
+       // console.log(e);
+        
+        // showtask();
+        let webtask = localStorage.getItem("localtask");
+        let taskObj = JSON.parse(webtask);
+        
+        let mytarget = e.target;
+        if(mytarget.classList[0] === 'text-success'){
+        let mytargetid = mytarget.getAttribute("id");
+        
+        
+        // let taskValue = taskObj[mytargetid]['task_name'];
+        
+        mytargetpresibling = mytarget.parentElement.previousElementSibling.previousElementSibling;
+            
+
+            for (keys in taskObj[mytargetid]) {
+                if(keys == 'completeStatus' && taskObj[mytargetid][keys]==true){
+                    taskObj[mytargetid].completeStatus = false;
+                   
+                }else if(keys == 'completeStatus' && taskObj[mytargetid][keys]==false){
+                    taskObj[mytargetid].completeStatus = true;
+                    
+                }
+              }
+        
+       // showtask();        
+        localStorage.setItem("localtask", JSON.stringify(taskObj));
+        showtask();
+    }
+    })
+
+    
+
+
+
+// deleteall
+let deleteallbtn = document.getElementById("deleteallbtn");
+deleteallbtn.addEventListener("click", function(){
+    let savetaskbtn = document.getElementById("savetaskbtn");
+    let addtaskbtn = document.getElementById("addtaskbtn");
+    let webtask = localStorage.getItem("localtask");
+    let taskObj = JSON.parse(webtask);
+    if(webtask == null){
+        taskObj = [];
+    }
+    else{
+        taskObj = JSON.parse(webtask);
+        taskObj = [];
+    }
+    savetaskbtn.style.display="none";
+    addtaskbtn.style.display="block";
+    localStorage.setItem("localtask", JSON.stringify(taskObj));
+    showtask();
+
+})
+
+
+// serachlist
+let searchtextbox = document.getElementById("searchtextbox");
+searchtextbox.addEventListener("input", function(){
+    let trlist = document.querySelectorAll("tr");
+    Array.from(trlist).forEach(function(item){
+        let searchedtext = item.getElementsByTagName("td")[0].innerText;
+        let searchtextboxval = searchtextbox.value;
+        let re = new RegExp(searchtextboxval, 'gi');
+        if(searchedtext.match(re)){
+            item.style.display="table-row";
+        }
+        else{
+            item.style.display="none";
+        }
+        
+    
+    })
+})
+searchtextbox.value="";
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
